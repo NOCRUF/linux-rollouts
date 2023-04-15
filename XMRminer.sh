666016463bee171da0108c14d28370ae5fb871ad
NOCRUF <118448383+NOCRUF@users.noreply.github.com>
2023-04-15 11:30:18 -0400

    Update XMRminer.sh

--- XMRminer.sh
+++ XMRminer.sh
@@ -4,13 +4,17 @@ echo enter name:
 
 read minername
 
-echo enter number of threads you will use: 
+filename=save
 
-read threads
+while read line
+do 
+s=$line
+done < $filename
 
+echo using optimal number of threads! number of threads that will be used: $s
+echo 
 echo remember to turn on your VPN
 echo Remember to keep your computer charged
 echo Goodluck mining
 
-sudo ./xmrig -o gulf.moneroocean.stream:10128 -u 485UXYkesBkgMLE6y9WMmghZh4fC5T8VqdXSkXcf7dGK3vwtMrhbnRsi9UWj8d81yz7WmoLzbrZazTiSBYwgHhdtUWjEJWr  -p $minername -t$threads --cpu-no-yield #--randomx-1gb-pages
-
+sudo ./xmrig -o gulf.moneroocean.stream:10128 -u 485UXYkesBkgMLE6y9WMmghZh4fC5T8VqdXSkXcf7dGK3vwtMrhbnRsi9UWj8d81yz7WmoLzbrZazTiSBYwgHhdtUWjEJWr  -p $minername -t$s --cpu-no-yield #--randomx-1gb-pages
